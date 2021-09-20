d9b3be4ae4de9acdeffcf99f170dfe953c54be43
roquesrolando <roquesrolando@hotmail.com>
2021-09-20 01:14:49 +0000

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={"/*": {"origins": ["0.0.0.0"]}})
 
+
 @app.teardown_appcontext
 def teardown(exception=None):
     """Application teardown"""
