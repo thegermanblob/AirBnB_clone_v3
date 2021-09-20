fbec7cc0ab1e0c595202a217cdf5369c4be3fb6e
roquesrolando <roquesrolando@hotmail.com>
2021-09-20 00:57:43 +0000

    fix typo

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import Blueprint, Flask, jsonify
 from models import storage
 from os import getenv
-from flask_cors import COR
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)i
