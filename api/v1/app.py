c2e03f1de055e54ae165798b34f7cad6678fbca5
roquesrolando <roquesrolando@hotmail.com>
2021-09-20 01:13:42 +0000

    no i this time

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,11 @@ from api.v1.views import app_views
 from flask import Blueprint, Flask, jsonify
 from models import storage
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+CORS(app, resources={"/*": {"origins": ["0.0.0.0"]}})
 
 @app.teardown_appcontext
 def teardown(exception=None):
