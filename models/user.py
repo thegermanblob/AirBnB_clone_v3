36ca3bfdb083b7db4c022511468d47fcd1e4c508
Thegermanblob <thegermanblob@gmail.com>
2021-09-20 22:47:47 -0400

    Add md5 encoding to user

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -26,4 +27,6 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if "password" in kwargs:
+            kwargs["password"] = hashlib.md5(kwargs["password"].encode()).hexdigest()
         super().__init__(*args, **kwargs)
